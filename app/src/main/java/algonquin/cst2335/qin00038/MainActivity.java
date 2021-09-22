988f194704ef953edeaf815605dfebe5aca8b4f4
ning <ning>
2021-09-22 15:27:43 -0400

    working on text widgets

--- app/src/main/java/algonquin/cst2335/qin00038/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/qin00038/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.qin00038;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button mybutton = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+        String editString = myedit.getText().toString();
+        mytext.setText( "Your edit text has: " + editString);
+        mybutton.setOnClickListener( vw -> mytext.setText("Your edit text has: " + editString) );
     }
 }
